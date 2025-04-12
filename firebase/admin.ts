661069926f7be79bcb8ddbd8af2df908a39c753e
kgaurav152 <kgaurav152@gmail.com>
2025-04-12 15:19:35 +0530

    modified admin.ts(firebase admin sdk) to accept base64 string as credential input in order to avoid deployment credential inaccessinbility issue

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -7,14 +7,22 @@ function initFirebaseAdmin() {
   const apps = getApps();
 
   if (!apps.length) {
+    
+    const base64 = process.env.FIREBASE_ADMIN_SDK_BASE64!;
+    const serviceAccount = JSON.parse(Buffer.from(base64, "base64").toString("utf8"));
+
     initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        // Replace newlines in the private key
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      }),
+      credential: cert(serviceAccount),
     });
+
+    // initializeApp({
+    //   credential: cert({
+    //     projectId: process.env.FIREBASE_PROJECT_ID,
+    //     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    //     // Replace newlines in the private key
+    //     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    //   }),
+    // });
   }
 
   return {
